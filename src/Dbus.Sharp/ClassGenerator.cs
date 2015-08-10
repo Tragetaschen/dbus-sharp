f482429307d572b28fa9710da6cb66882f6df65b
Kai Ruhnau <kai.ruhnau@target-sg.com>
2015-08-10 17:24:36 +0200

    Started a code generator

---
+++ src/Dbus.Sharp/ClassGenerator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Framework.Runtime.Roslyn;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dbus.Sharp
+{
+    public class ClassGenerator : ICompileModule
+    {
+        public void BeforeCompile(BeforeCompileContext context)
+        {
+            Console.WriteLine("I am running");
+        }
+
+        public void AfterCompile(AfterCompileContext context)
+        {
+        }
+    }
+}

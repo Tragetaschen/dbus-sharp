02b93de21c2d95f1fb1ffd37d9afcd8795aecd8c
Kai Ruhnau <kai.ruhnau@target-sg.com>
2015-08-10 20:06:17 +0200

    Extracted all types with the Interface attribute

--- src/Dbus.Sharp/ClassGenerator.cs
+++ src/Dbus.Sharp/ClassGenerator.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
 
 namespace Dbus.Sharp
 {
@@ -10,7 +11,40 @@ namespace Dbus.Sharp
     {
         public void BeforeCompile(BeforeCompileContext context)
         {
-            Console.WriteLine("I am running");
+            recurseNamespace(context.Compilation.GlobalNamespace);
+            Console.WriteLine("I was running");
+        }
+
+        private void recurseNamespace(INamespaceSymbol ns)
+        {
+            foreach (var type in ns.GetTypeMembers())
+                handleType(type);
+            foreach (var subNamespace in ns.GetNamespaceMembers())
+                recurseNamespace(subNamespace);
+        }
+
+        private void handleType(ITypeSymbol type)
+        {
+            var interfaceAttributeArgument = type
+                .GetAttributes()
+                .FirstOrDefault(x => x.AttributeClass.Name == "InterfaceAttribute")
+                ?.ConstructorArguments
+                .First()
+            ;
+            if (interfaceAttributeArgument.HasValue)
+            {
+                generateCodeFor(type, (string)interfaceAttributeArgument.Value.Value);
+                foreach (var subType in type.GetTypeMembers())
+                    handleType(subType);
+            }
+        }
+
+        private void generateCodeFor(ITypeSymbol type, string value)
+        {
+            Console.WriteLine("Generated" + type.Name);
+            foreach (var member in type.GetMembers())
+                Console.WriteLine(" " + member.Name + " " + member);
+            Console.WriteLine();
         }
 
         public void AfterCompile(AfterCompileContext context)
